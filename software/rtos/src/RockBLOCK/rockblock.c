7d5e063cf7d240c68d246ad2567b55c436e22635
ssscrazy <ssscrazy@umich.edu>
2019-11-10 18:20:31 -0500

    wrote rockblock driver. Need to test

--- software/rtos/src/RockBLOCK/rockblock.c
+++ software/rtos/src/RockBLOCK/rockblock.c
@@ -2,3 +2,315 @@
 
 
 #include "rockblock.h"
+
+// formats the command for the message sent to the RockBLOCK.
+static void rb_format_command(ROCKBLOCK_t *rb, rb_command_t cmd) {
+
+    while(rb->tx.transmitting);
+
+    *(rb->tx.cur_ptr++) = 'A';
+    *(rb->tx.cur_ptr++) = 'T';
+
+    if(cmd != AT && cmd != ATK0) {
+        *(rb->tx.cur_ptr++) = '+';
+        *(rb->tx.cur_ptr++) = 'S';
+        *(rb->tx.cur_ptr++) = 'B';
+        *(rb->tx.cur_ptr++) = 'D';
+    }
+
+    switch(cmd) {
+        case AT:
+        break;
+        case ATK0:
+            *(rb->tx.cur_ptr++) = '&';
+            *(rb->tx.cur_ptr++) = 'K';
+            *(rb->tx.cur_ptr++) = '0';
+        break;
+        case SBDWT:
+            *(rb->tx.cur_ptr++) = 'W';
+            *(rb->tx.cur_ptr++) = 'T';
+        break;
+        case SBDIX:
+            *(rb->tx.cur_ptr++) = 'I';
+            *(rb->tx.cur_ptr++) = 'X';
+        break;
+        case SBDRT:
+            *(rb->tx.cur_ptr++) = 'R';
+            *(rb->tx.cur_ptr++) = 'T';
+        break;
+        case SBDRB:
+            *(rb->tx.cur_ptr++) = 'R';
+            *(rb->tx.cur_ptr++) = 'B';
+        break;
+    }
+    // set end of command index and add the carriage return or equal sign depending on command.
+    rb->tx.last_ptr = rb->tx.cur_ptr;
+
+    if(cmd == SBDWT)
+        *(rb->tx.cur_ptr++) = '='; // want an '=' sign here, after this the message will follow.
+    else
+        *(rb->tx.cur_ptr++) = '\r'; // should be 0x0D, same as '\r' hopefully.
+}
+
+static void rb_clear_buffers(ROCKBLOCK_t *rb) {
+    rb->tx.cur_ptr = rb->tx.buff;
+    rb->tx.last_ptr = rb->tx.buff;
+    rb->rx.cur_ptr = rb->rx.buff;
+    rb->rx.last_ptr = rb->rx.buff;
+}
+
+void rb_init(ROCKBLOCK_t *rb) {
+
+    // initialize buffers with appropriate start values and end values.
+    rb->rx.cur_ptr = rb->rx.buff;
+    rb->rx.last_ptr = rb->rx.buff;
+    rb->rx.end_ptr = rb->rx.buff + RB_RX_SIZE - 1;
+
+    rb->tx.cur_ptr = rb->tx.buff;
+    rb->tx.last_ptr = rb->tx.buff;
+    rb->tx.end_ptr = rb->tx.buff + RB_TX_SIZE - 1;
+
+    // UART initialization
+    UARTConfig a1_cnf = {
+                    .moduleName = USCI_A1,
+                    .portNum = PORT_5,
+                    .RxPinNum = PIN7,
+                    .TxPinNum = PIN6,
+                    .clkRate = configCPU_CLOCK_HZ,
+                    .baudRate = 19200L,
+                    .clkSrc = UART_CLK_SRC_SMCLK,
+                    .databits = 8,
+                    .parity = UART_PARITY_NONE,
+                    .stopbits = 1
+    };
+
+    // not using ring buffer, so these are null. All TX/RX choices are processed by callback function in this file
+    initUSCIUart(&a1_cnf, NULL, NULL);
+    initUartRxCallback(&USCI_A1_cnf, &rb_rx_callback, rb);
+    initUartTxCallback(&USCI_A1_cnf, &rb_tx_callback, rb);
+
+    // ring, network-available, and sleep pin initialization
+    P8DIR &= ~(BIT0 | BIT1); // set ring and network-available pins to inputs.
+
+    P7DIR |= BIT3; // set sleep to an output.
+
+    rb_set_awake(true); // TODO: might want to not just always be on, so add the ability to sleep...
+
+    rb->tx.transmitting = false;
+}
+
+
+void rb_set_awake(bool awake) {
+    if(awake)
+        P7OUT |= BIT3; // set 7.3 high
+    else
+        P7OUT &= ~BIT3; // set 7.3 low
+}
+
+bool rb_check_ring(void) {
+    if(P8IN & BIT0) // get all input statuses on port 8, and check specifically bit0 (RI)
+        return false; // RI is active low, so if the pin is high we must not have a ring.
+    else
+        return true; // it was 0 (Active low), so we have a ring.
+}
+
+bool rb_check_netav(void){
+    if(P8IN & BIT1) // checking P8.1, which is where NETAV pin is connected.
+        return true; // this pin is active high.
+    else
+        return false; // no signal at the time.
+}
+
+void rb_rx_callback(void *param, uint8_t datum) {
+    static uint8_t numReturns = 0;
+    ROCKBLOCK_t *rb = (ROCKBLOCK_t *) param;
+
+    *(rb->rx.cur_ptr) = datum; // take the data, we assume we have room here.
+
+    if(datum == (uint8_t) '\r') { // all message responses end with '\r', but there might be '\r' per message.
+        numReturns++;
+
+        if(numReturns == rb->rx.numReturns) {
+            numReturns = 0;
+            rb->rx.finished = true;
+            rb->rx.last_ptr = rb->rx.cur_ptr;
+        }
+    }
+
+    rb->rx.cur_ptr++; // increment the index.
+
+    if(rb->rx.cur_ptr >= rb->rx.end_ptr) { // we are full! Message must be complete.
+        //TODO: Handle this case in a reasonable way.
+    }
+}
+
+void rb_tx_callback(void *param, uint8_t *txAddress) {
+    ROCKBLOCK_t *rb = (ROCKBLOCK_t *) param;
+
+    *txAddress= *(rb->tx.cur_ptr);
+
+    if(rb->tx.cur_ptr == rb->tx.last_ptr) { // sent last byte.
+        rb->tx.transmitting = false;
+    }
+
+    rb->tx.cur_ptr++;
+}
+
+void rb_send_message(ROCKBLOCK_t *rb, uint8_t * msg, uint16_t len, bool *msgSent, int8_t *msgReceived, int8_t *msgsQueued) {
+
+    rb_clear_buffers(rb);
+
+    // create the message.
+    rb_format_command(rb, SBDWT);    // Set up the command
+
+    uint16_t i = 0;
+    for(i = 0; i < len; i++) {
+        *(rb->tx.cur_ptr++) = *(msg+i);
+    }
+
+    rb->tx.last_ptr = rb->tx.cur_ptr;
+    *(rb->tx.cur_ptr++) == '\r'; // end message with carriage return.
+
+    rb->tx.transmitting = true; // we are transmitting
+    rb->rx.finished = false; // not done receiving response
+    rb->rx.numReturns = 1; // expect 1 carriage return to signify end of comms
+    uint16_t totalLen = rb->tx.last_ptr - rb->tx.buff; // length
+    uartSendDataInt(&USCI_A1_cnf, rb->tx.buff, totalLen);
+    while(!rb->rx.finished); // wait for response.
+
+    rb_start_session(rb, msgSent, msgReceived, msgsQueued);
+
+    // process response to see if message succeeded.
+
+}
+
+void rb_start_session(ROCKBLOCK_t *rb, bool *msgSent, int8_t *msgReceived, int8_t *msgsQueued) {
+    // start session.
+
+    rb_clear_buffers(rb);
+
+    rb_format_command(rb, SBDIX);
+    rb->tx.transmitting = true;
+    rb->rx.finished = false;
+    rb->rx.numReturns = 1; // I expect just one '/r' to signify end of message.
+    uint16_t totalLen = rb->tx.last_ptr - rb->tx.buff;
+
+    uartSendDataInt(&USCI_A1_cnf, rb->tx.buff, totalLen);
+
+    while(!rb->rx.finished); // wait for response
+
+    totalLen = rb->rx.last_ptr - rb->rx.buff;
+
+    *msgSent = true;
+    *msgReceived = 0;
+
+    if(rb->rx.numReturns != 1) {
+        // ERROR
+        *msgSent = false;
+        return;
+    }
+
+    // response format:
+    //+SBDIX: <MO status>, <MOMSN>, <MT status>, <MTMSN>, <MT length>, <MT queued>\r
+
+    uint16_t i = 0;
+    uint8_t numField = 0;
+    uint8_t numDigits = 0;
+
+    for(i = 0; i < totalLen; i++) {
+        switch(numField) {
+            case 0: // SBDIX header
+                if(rb->rx.buff[i] == ':')
+                    numField++;
+            break;
+            case 1: // msgSent (MO). Should be 0, 1, or 2.
+                if(rb->rx.buff[i] == ' ')
+                    continue;
+                else if(rb->rx.buff[i] == ',') {
+                    numDigits = 0;
+                    numField++;
+                } else if(rb->rx.buff[i] == '0' || rb->rx.buff[i] == '1' || rb->rx.buff[i] == '2') {
+                    if(numDigits == 0) {
+                        *msgSent = true;
+                        numDigits = 1;
+                    } else {
+                        *msgSent = false;
+                        numDigits = 1;
+                    }
+                } else { // received anything weird, we failed.
+                    *msgSent = false;
+                    numDigits = 1; //if its greater than 0, and we go through the loop again, msgSent can't go true. Which is what we want.
+                }
+            break;
+            case 2: // don't care.
+                if(rb->rx.buff[i] == ',')
+                    numField++;
+                else
+                    continue;
+            break;
+            case 3: // receive status (MT). 0 -> no message received, 1 -> message received, 2 -> error.
+                if(rb->rx.buff[i] == ',')
+                    numField++;
+                else if(rb->rx.buff[i] == ' ')
+                    continue;
+                else if(rb->rx.buff[i] == '0')
+                    *msgReceived = 0;
+                else if(rb->rx.buff[i] == '1')
+                    *msgReceived = 1;
+                else // we are using -1 as an error output from this function. The number 2 in this field from the message means an error, though.
+                    *msgReceived = -1;
+            break;
+            case 4: //don't care
+                if(rb->rx.buff[i] == ',')
+                    numField++;
+                else
+                    continue;
+            break;
+            case 5: // size of downloaded message, don't care currently.
+                if(rb->rx.buff[i] == ',')
+                    numField++;
+                else
+                    continue;
+            break;
+            case 6: // number of messages in queue waiting to be downloaded.
+                if(*msgReceived == -1 || *msgReceived == 0)
+                    continue;
+                else {
+                    int j = i;
+                    for(j = i; j < totalLen; j++) {
+                        if(rb->rx.buff[i] == '\r' || j == totalLen - 1) {
+                            rb->rx.buff[i] = '\0';
+                            break;
+                        }
+                    }
+                    *msgsQueued = atoi(rb->rx.buff + i);
+                    i = totalLen;
+                }
+            break;
+        }
+    }
+}
+
+uint8_t rb_check_mailbox(ROCKBLOCK_t *rb, int8_t *msgsQueued) {
+    bool msgSent;
+    int8_t msgReceived;
+    rb_clear_buffers(rb);
+    rb_send_message(rb, NULL, 0, &msgSent, &msgReceived, msgsQueued);
+    return msgReceived;
+}
+
+void rb_retrieve_message(ROCKBLOCK_t *rb) {
+
+    rb_clear_buffers(rb);
+    rb_format_command(rb, SBDRT);
+    uint16_t totalLen = rb->tx.last_ptr - rb->tx.buff; // length
+
+    rb->tx.transmitting = true;
+    rb->rx.finished = false;
+    rb->rx.numReturns = 3; // I expect 3 '/r' to signify end of message.
+
+    uartSendDataInt(&USCI_A1_cnf, rb->tx.buff, totalLen);
+
+    while(!rb->rx.finished); // wait for response.
+
+}
