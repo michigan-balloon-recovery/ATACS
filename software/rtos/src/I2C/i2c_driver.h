b435acb065afd9f173862fd1d8971e0014491af8
kapel34 <55847037+kapel34@users.noreply.github.com>
2019-12-09 21:10:18 -0500

    commenting i2c

--- software/rtos/src/I2C/i2c_driver.h
+++ software/rtos/src/I2C/i2c_driver.h
@@ -7,32 +7,63 @@
 
 #ifndef I2C_DRIVER_H_
 #define I2C_DRIVER_H_
+
+// -------------------------------------------------------------- //
+// -------------------- include dependencies -------------------- //
+// -------------------------------------------------------------- //
+
+//standard libraries
 #include <stdint.h>
+#include <stdbool.h>
+//FreeRTOS
 #include "FreeRTOS.h"
 #include "semphr.h"
-#include <stdbool.h>
 
 SemaphoreHandle_t i2c_rx_semaphore;
 SemaphoreHandle_t i2c_busy_semaphore;
 SemaphoreHandle_t i2c_tx_semaphore;
 static char status;
 
-// sets up the i2c and all of its configurations. Enables all interrupts
-// must be called before write or read are ever called.
+// ----------------------------------------------------------- //
+// -------------------- public prototypes -------------------- //
+// ----------------------------------------------------------- //
+
+/*!
+ * \sets up the i2c and all of its configurations. Enables all interrupts
+ *  must be called before write or read are ever called.
+ *
+ * \return None 
+ */
 int i2c_setup(void);
 
-// sends a message over the i2c bus
-// returns true if the message was successfully sent
+/*!
+ * \sends a message over the i2c bus
+ *
+ * \return true if successfully transmitted the data
+ */
 bool i2c_write(uint8_t addr, uint8_t * data, uint8_t numBytes);
 
-// reads a message over the i2c bus
-// returns true if the message was successfully sent and a response was received
+/*!
+ * \reads a message over the i2c bus
+ *
+ * \return true if successfully received the data
+ */
 bool i2c_read(uint8_t addr, uint8_t * data, uint8_t numBytes);
 
-// disables all interrupts associated with i2c. Returns true if it successfully disabled interrupts
+
+/*!
+ * \disables all interrupts associated with i2c. Returns true if it successfully disabled interrupts
+ * 
+ * \return true if successfully disabled interrupts 
+ */
 bool i2c_disable_interrupts(void);
 
-// enables all interrupts associated with the i2c bus. Do not call unless you used i2c_disable_interrupts() successfully prior to calling this.
+/*!
+ * \enables all interrupts associated with the i2c bus. 
+ *  Do not call unless you used i2c_disable_interrupts() successfully prior to calling this.
+ *
+ * \return None
+ */
 void i2c_enable_interrupts(void);
 
 __interrupt void USCI_B0_ISR(void);
